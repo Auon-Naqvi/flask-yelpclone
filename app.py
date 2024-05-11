644ab4b1790ab702a31bd2d22e4787786c796aaa
2240631 <166353372+2240631@users.noreply.github.com>
2024-05-11 12:43:06 -0500

    define submit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request 
 import my_yelp
 
 app = Flask(__name__)
@@ -12,12 +12,12 @@ def index():
 def submit():
     user_submitted_term = request.values.get("term")
     user_submitted_location = request.values.get("location")
-    results = my_yelp.search_businesses(user_submitted_term, user_submitted_location)
+    results = my_yelp.search_businesses(user_submitted_term, user_submitted_location) 
     
     return render_template("display_results.html", establishments=results, term=user_submitted_term, location=user_submitted_location)
 
 # # Automatically reload the application upon a template file change
-app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 #run the application    
 if __name__ == "__main__":
